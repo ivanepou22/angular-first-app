c43f0f8d3d6c85904a306332efb0c44b2ecee424
Ivan Epou <ivanepou92@gmail.com>
2023-01-23 23:55:40 +0300

    Update the books

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookListComponent } from './book-list/book-list.component';
+import { BookComponent } from './book/book.component';
 
 
 
 @NgModule({
   declarations: [
-    BookListComponent
+    BookListComponent,
+    BookComponent
   ],
   imports: [
     CommonModule
